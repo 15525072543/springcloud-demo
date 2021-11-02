fcafbe1644e5d7dca8c7e3673f47aeae6379dedf
wangtao <523036053@qq.com>
2021-11-02 17:27:54 +0800

    springboot 整合springmvc

---
+++ springboot-user/src/main/java/com/study/UserApplication.java
@@ -0,0 +1,15 @@
+package com.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * springboot的启动类
+ */
+@SpringBootApplication
+public class UserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserApplication.class,args);
+    }
+}
