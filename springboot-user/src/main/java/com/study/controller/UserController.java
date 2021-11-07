07f13d4fa402e622569e923eeea7cd234eec5eea
15525072543 <349415216@qq.com>
2021-11-08 00:53:00 +0800

    体验thymeleaf

--- springboot-user/src/main/java/com/study/controller/UserController.java
+++ springboot-user/src/main/java/com/study/controller/UserController.java
@@ -3,12 +3,14 @@ package com.study.controller;
 import com.study.pojo.User;
 import com.study.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
 @RequestMapping("user")
 public class UserController{
 
@@ -16,12 +18,22 @@ public class UserController{
     private UserService userService;
 
     @GetMapping("test")
+    @ResponseBody
     public String test(){
         return "hello user";
     }
 
     @GetMapping("{id}")
+    @ResponseBody
     public User queryUser(@PathVariable("id") Long id){
         return this.userService.queryUserById(id);
     }
+
+    @GetMapping("all")
+    public String toUsers(Model model){
+        List<User> users = this.userService.queryUserAll();
+        model.addAttribute("users", users);
+        return "users";
+    }
+
 }
--- springboot-user/src/main/java/com/study/service/UserService.java
+++ springboot-user/src/main/java/com/study/service/UserService.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Service
 public class UserService {
 
@@ -20,4 +22,9 @@ public class UserService {
     public void deleteUserById(Long id){
         this.userMapper.deleteByPrimaryKey(id);
     }
+
+    public List<User> queryUserAll(){
+        return this.userMapper.selectAll();
+    }
+
 }
