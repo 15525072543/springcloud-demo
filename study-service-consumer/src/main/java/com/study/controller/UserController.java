f089ca81433ba487e2bfa61256fb7d29350603f5
15525072543 <349415216@qq.com>
2021-11-29 22:45:00 +0800

    spirngcloud高级配置

--- study-service-consumer/src/main/java/com/study/controller/UserController.java
+++ study-service-consumer/src/main/java/com/study/controller/UserController.java
@@ -1,10 +1,13 @@
 package com.study.controller;
 
 import com.study.pojo.User;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * @ClassName: UserController
@@ -19,9 +22,16 @@ public class UserController {
     @Resource
     private RestTemplate restTemplate;
 
+    @Resource
+    // 包含了拉取服务的所有信息
+    private DiscoveryClient discoveryClient;
+
     @GetMapping
     public User queryUserById(@RequestParam("id") Long id){
-        return this.restTemplate.getForObject("http://localhost:8081/user/" + id,User.class);
+        // 根据服务名创建服务实例
+        List<ServiceInstance> instances = discoveryClient.getInstances("service-provider");
+        ServiceInstance instance = instances.get(0);
+        return this.restTemplate.getForObject("http://"+ instance.getHost() +":"+ instance.getPort() +"/user/" + id,User.class);
     }
 
 }
